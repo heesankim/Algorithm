d48f7cfb917fd2f0d37ed1518fd11e5d2a3554ae
Heesan Kim <opqryt45@gmail.com>
2022-10-12 21:14:07 +0900

    update

--- week03/14888/14888.py
+++ week03/14888/14888.py
@@ -1,3 +1,13 @@
+# 입력받는  + , - , x , / 연산자의 갯수를 저장해놓고 시작한다. 어떻게?
+# 카운트리스트를 만들거나 아니면 전부다 따로 하나씩 cnt를 만들거나 한다.
+# 함수의 인자로는 처음의 주어진 값이 들어간다 . (1, cnt,cnt,cnt,cnt ... )
+# 함수의 파라미터에 깊이를 인자로 주어서 함수 종료조건을 설정해줌
+# 그리고 함수가 종료될때 계산된 값을 리스트에 저장한다.
+
+# 출력
+# 최댓값
+# 최솟값
+
 import sys
 input = sys.stdin.readline
 
@@ -5,23 +15,15 @@ maximum = -1e9
 minimum = 1e9
 N = int(input())
 
-# 입력받는  + , - , x , / 연산자의 갯수를 저장해놓고 시작한다. 어떻게?
-# 카운트리스트를 만들거나 아니면 전부다 따로 하나씩 cnt를 만들거나 한다.
-# 함수의 인자로는 처음의 주어진 값이 들어간다 . (1, cnt,cnt,cnt,cnt ... )
-# 그리고 함수가 종료되는 조건은 만약에 마지막 번째 값이 들어가거나 모든 연산자의 cnt가 0이 되었을 때 이다.(합이 0? 리스트라면)
-# 그리고 함수가 종료될때 계산된 값을 리스트에 저장한다.
-
-
 nodes = list(map(int, input().split()))
 
-value_list = []
-operator_list = list(map(int, input().split()))
+operator_list = list(map(int, input().split())) # 연산자의 갯수를 리스트에 담아 놓는다.
 
 
 def dfs(depth, cur, plus_cnt, minus_cnt, mult_cnt, divide_cnt):
     global maximum
     global minimum
-    if depth == N:
+    if depth == N: # 함수 종료 조건
         if maximum < cur:
             maximum = cur
         if minimum > cur:
