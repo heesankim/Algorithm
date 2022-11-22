173e9ce1549efc4a70cac5ee13035bc181c88d02
Heesan Kim <opqryt45@gmail.com>
2022-11-22 13:22:17 +0900

    update

---
+++ personal/22_11_22/2721/2721.py
@@ -0,0 +1,16 @@
+import sys
+
+input = sys.stdin.readline
+
+T = int(input())
+
+for i in range(T):
+    n = int(input())
+    T = 0
+    sum = 0
+    for j in range(1, n+1):
+        for k in range(1, j+2):
+            T = T + k
+            sum = sum + j*T
+            T = 0
+    print(sum)
