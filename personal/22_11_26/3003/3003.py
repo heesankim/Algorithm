693ef41132afa95f45dfa93f0cb101bc8c8b0460
Heesan Kim <opqryt45@gmail.com>
2022-11-26 16:18:57 +0900

    docs : 22_11_26 algorithm

---
+++ personal/22_11_26/3003/3003.py
@@ -0,0 +1,9 @@
+import sys
+
+input = sys.stdin.readline
+
+king, queen, rook, bishop, knight, pawn = map(int, input().split())
+
+print((1-king), (1-queen), (2-rook), (2-bishop), (2-knight), (8-pawn))
+
+
