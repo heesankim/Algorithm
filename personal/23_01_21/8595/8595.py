7384860e43f45030339d51ec10975e128b4f5409
heesankim <opqryt45@gmail.com>
2023-02-02 00:12:08 +0900

    11728 배열합치기

--- personal/23_01_21/8595/8595.py
+++ personal/23_01_21/8595/8595.py
@@ -1,16 +1,23 @@
 # 히든 넘버   @ 시간초과 뜸... ㅠㅠ
+# 최대한 효울적으로 짜자 -> 복잡도 고려
+# 코드가 돌아간다고 만사 아님
+
 import sys
 input = sys.stdin.readline
 
-N = int(input())
-
-word = input()
-
-res = 0
-for i in word:
-    if i.isdigit():
-        
-        
-
-
-# print(sum(list(map(int, hiddenNum.split()))))
+input()
+s = input()
+hidden_numbers = []
+is_number = False
+num = ""
+for i in s:
+    if i not in ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']:
+        if len(num) < 7 and len(num) != 0:
+            hidden_numbers.append(int(num))
+        num = ""
+        continue
+    num += i
+if len(num) < 7 and len(num) != 0:
+        hidden_numbers.append(int(num))
+print(sum(hidden_numbers))
+# 아스키 코드 크기 비교로 풀어도 됨
