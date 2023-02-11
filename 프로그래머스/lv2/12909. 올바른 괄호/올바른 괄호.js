6a8c8179f30e46f740404d11a8921bf4620c9ae1
heesan <75322004+heesankim@users.noreply.github.com>
2023-02-12 00:42:11 +0900

    [level 2] Title: 올바른 괄호, Time: 5.43 ms, Memory: 37.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.js
@@ -0,0 +1,21 @@
+function solution(s){
+    if(s.length % 2 ===1 ){
+        return false
+    }
+    let stack = []
+    for(let i = 0; i < s.length; i++){
+        if(s[i] === "("){
+            stack.push(s[i])
+        }else if(s[i] === ")"){
+            if(stack.length === 0){
+                return false
+            }else{
+                stack.pop()
+            }
+        }
+    }
+    if(stack.length !== 0){
+        return false
+    }
+    return true;
+}
