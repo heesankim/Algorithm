1fdbef94e47fbbe883efdf4c8640d6f7920dace6
heesan <75322004+heesankim@users.noreply.github.com>
2023-02-18 03:07:05 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (1), Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.js
@@ -0,0 +1,12 @@
+function solution(my_string) {
+    var answer = 0;
+    for(let i = 0; i < my_string.length; i++){
+        if(isNaN(my_string[i])){
+            continue
+        }
+        else{
+            answer = answer + Number(my_string[i])
+        }
+    }
+    return answer
+}
