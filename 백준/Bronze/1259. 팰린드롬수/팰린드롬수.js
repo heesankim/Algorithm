2f6a4adac6d37140d7a09fb37aec8287079ce2af
heesan <75322004+heesankim@users.noreply.github.com>
2023-02-20 19:56:44 +0900

    [Bronze I] Title: 팰린드롬수, Time: 124 ms, Memory: 9420 KB -BaekjoonHub

---
+++ 백준/Bronze/1259. 팰린드롬수/팰린드롬수.js
@@ -0,0 +1,10 @@
+const fs = require("fs");
+const input = fs.readFileSync("/dev/stdin").toString().trim().split("\n")
+input.pop();
+
+for (let i = 0; i < input.length; i++) {
+  let reverseStr = input[i].split("").reverse().join("");
+  // console.log(reverseStr);
+  console.log(input[i] === reverseStr ? "yes" : "no");
+}
+
