a3e1ea112783d997704df4dc87432bc75fa30a9c
heesan <75322004+heesankim@users.noreply.github.com>
2023-02-21 20:20:20 +0900

    [Bronze III] Title: 전자레인지, Time: 172 ms, Memory: 9640 KB -BaekjoonHub

---
+++ 백준/Bronze/10162. 전자레인지/전자레인지.js
@@ -0,0 +1,22 @@
+const fs = require("fs");
+const input = fs
+  .readFileSync("/dev/stdin")
+  .toString()
+  .trim()
+  .split()
+  .map(Number);
+// const fs = require("fs");
+// const input = fs.readFileSync("/dev/stdin").toString().trim();
+
+function solve(input) {
+  if (input % 10 !== 0) return -1;
+  let result = [];
+  result.push(parseInt(input / 300));
+  input = input % 300;
+  result.push(parseInt(input / 60));
+  input = input % 60;
+  result.push(parseInt(input / 10));
+  result.join(" ")
+  return result.join(" ")
+}
+console.log(solve(input))
