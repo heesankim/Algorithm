0fe86185d0bbb707eca21c6aeb645e09774cd8ef
heesan <75322004+heesankim@users.noreply.github.com>
2023-03-09 21:48:51 +0900

    [level 0] Title: 합성수 찾기, Time: 0.08 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120846. 합성수 찾기/합성수 찾기.js
+++ 프로그래머스/lv0/120846. 합성수 찾기/합성수 찾기.js
@@ -1,42 +1,4 @@
 function solution(n) {
-    
-    const measure = (a) => {
-    let cnt = 0
-    let answer = false
-    for(let i = 1; i <= a; i++){
-        if(a % i === 0){
-            cnt++
-        }
-    }
-    if(cnt >= 3){
-        answer = true
-    }
-        return answer
-    }
-    let cnt = 0;
-    for(let i = 4; i <= n; i++){
-        if(measure(i)){
-            cnt = cnt + 1
-        }
-    }
-    return cnt;
-}
-
-// 약수의 개수가 세 개 이상인 수를 합성 수 
-
-
-
-// 합성수인지 판별하는 함수
-function measure(num) {
-    let count = 0
-    let answer = false
-    for(let i = 1; i <= num; i++){
-        if(num % i === 0){
-            count++
-        }
-    }
-    if(count >= 3){
-        answer = true
-    }
-    return answer
+    var answer = 0;
+    return answer;
 }
