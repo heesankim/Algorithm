a3b63bc34790d8fec23634319c17c7b042e89aa4
heesan <75322004+heesankim@users.noreply.github.com>
2023-03-20 23:27:13 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (2), Time: 0.07 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.js
+++ 프로그래머스/lv0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.js
@@ -1,20 +1,10 @@
 function solution(my_string) {
     let sum = 0;
-    let str = ""
-    let temp = ""
+    const numStr = "123456789"
     for(let i = 0; i < my_string.length; i++){
-        temp = my_string[i]
-        if(!isNaN(temp)) { // 만약 숫자형태라면
-           str = str + temp // 일단 임시로 문자로 저장
-            console.log(str)
-        }
-        else { // 숫자형태가 아니라면
-            sum = sum + Number(str) // 저장되어 있는 str을 숫자로변환해서 sum에 더해줌
-            str = ""
-        }
-        if(i === my_string.length - 1){
-            sum = sum + Number(str)
+        if(numStr.includes(my_string[i])){ 
+           sum = sum + Number(my_string[i])
         }
     }
-    return sum
+    return sum;
 }
