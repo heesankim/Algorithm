6aab1ebaafff09217a8de7365991d25eeda817d7
heesan <75322004+heesankim@users.noreply.github.com>
2023-05-04 00:49:36 +0900

    [unrated] Title: 글자 지우기, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181900. 글자 지우기/글자 지우기.js
+++ 프로그래머스/unrated/181900. 글자 지우기/글자 지우기.js
@@ -1,10 +1,12 @@
 function solution(my_string, indices) {
     let answer = ''
-    indices.sort((a,b) => a - b)
+    // indices.sort((a,b) => a - b)
     for(let i = 0 ; i < my_string.length; i ++){
         if(!indices.includes(i)){
-            answer = answer + my_string[i]
+            // indices 배열에 i가 없으면(false)
+            answer = answer + my_string[i] // 빈문자열에 더해라 
         }
     }
     return answer;
 }
+let arr = [1,2,3]
