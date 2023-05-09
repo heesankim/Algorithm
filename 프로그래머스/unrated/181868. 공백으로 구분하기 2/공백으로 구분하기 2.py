8694350c81e74b241d6e8b66531dbb71b21626b2
heesan <75322004+heesankim@users.noreply.github.com>
2023-05-09 22:37:25 +0900

    [unrated] Title: 공백으로 구분하기 2, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/181868. 공백으로 구분하기 2/공백으로 구분하기 2.py
+++ 프로그래머스/unrated/181868. 공백으로 구분하기 2/공백으로 구분하기 2.py
@@ -1,10 +1,17 @@
-def solution(my_string):
-    answer = []
-    new_str = my_string.split(" ")
-    for i in new_str:
-        if i == "":
-            continue
-        else:
-            answer.append(i)
+# def solution(my_string):
+#     answer = []
+#     new_str = my_string.split(" ")
+#     for i in new_str:
+#         if i == "":
+#             continue
+#         else:
+#             answer.append(i)
     
+#     return answer
+
+
+
+def solution(my_string):
+    answer = my_string.split()
+    print(answer)
     return answer
