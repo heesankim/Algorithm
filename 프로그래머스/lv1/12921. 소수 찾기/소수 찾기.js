43ab563ffbc4cd6c2ea355009d031b4713afe8a0
heesan <75322004+heesankim@users.noreply.github.com>
2023-05-10 01:15:40 +0900

    [level 1] Title: 소수 찾기, Time: 22.22 ms, Memory: 50 MB -BaekjoonHub

--- 프로그래머스/lv1/12921. 소수 찾기/소수 찾기.js
+++ 프로그래머스/lv1/12921. 소수 찾기/소수 찾기.js
@@ -1,41 +1,43 @@
-function solution(n) {
-    
-    let count = 1; 
-    
-    for(let i=2; i<=n; i++){ 
-         if(isPrime(i)) count++;
-    }
 
-
-    function isPrime(num){
-        if(num % 2===0) return false;
-        for(let i=2; i<Math.sqrt(num)+1; i++){
-            if(num%i==0) return false; 
+let solution = (n) => {
+    let arr = Array(n + 1).fill(true).fill(false, 0, 2);
+ 
+    for (let i = 2; i < Number(n ** 0.5) + 1; i++) {
+        if (arr[i]) {
+            for (let j = i * i; j <= n; j += i) {
+                arr[j] = false;
+                
+            }
         }
-        return true;
     }
-
-    return count;
+let count = 0
+    arr.map(item =>{
+        if(item === true){
+            count++
+        }
+    });
+    return count
 }
 
-// let solution = (n) => {
-//     let arr = Array(n + 1).fill(true).fill(false, 0, 2);
- 
-//     for (let i = 2; i < Number(n ** 0.5) + 1; i++) {
-//         if (arr[i]) {
-//             for (let j = i * i; j <= n; j += i) {
-//                 arr[j] = false;
-                
-//             }
-//         }
-//     }
-// let count = 0
-//     arr.map(item =>{
-//         if(item === true){
-//             count++
-//         }
-//     });
-//     return count
-// }
+console.log(solution(10000));
 
-// console.log(solution(10000));
+
+// function solution(n) {
+    
+//     let count = 1; 
+    
+//     for(let i=2; i<=n; i++){ 
+//          if(isPrime(i)) count++;
+//     }
+
+
+//     function isPrime(num){
+//         if(num % 2 === 0) return false;
+//         for(let i = 2; i < Math.sqrt(num)+1; i++){
+//             if(num%i==0) return false; 
+//         }
+//         return true;
+//     }
+
+//     return count;
+// }
