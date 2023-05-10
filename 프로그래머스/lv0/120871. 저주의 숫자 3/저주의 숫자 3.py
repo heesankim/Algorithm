c250d776d63b54d2e4536a99b768985a66c2a23f
heesan <75322004+heesankim@users.noreply.github.com>
2023-05-10 23:20:26 +0900

    [level 0] Title: 저주의 숫자 3, Time: 0.15 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120871. 저주의 숫자 3/저주의 숫자 3.py
+++ 프로그래머스/lv0/120871. 저주의 숫자 3/저주의 숫자 3.py
@@ -1,12 +1,26 @@
 def solution(n):
     arr = []
     newArr = []
+    delete = 0
     for i in range(1,200):
         arr.append(i)
     for i in arr:
         if str(i).find("3") != -1 or i % 3 == 0:
+            delete = delete + 1
             continue
         else:
             newArr.append(i)
-    # print(newArr)
-    return newArr[n-1]
+    return newArr[n - 1]
+
+# 모범답안
+# def solution(n):
+#     answer = 0
+#     for _ in range(n):
+#         answer += 1
+
+#         print("첫 answer", answer)
+#         while answer % 3 == 0 or '3' in str(answer):
+#             answer += 1
+#             print(answer)
+            
+#     return answer
