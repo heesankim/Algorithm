99fc701c9d96aef05c77e8a11ff5102028cbabd0
heesan <75322004+heesankim@users.noreply.github.com>
2023-05-16 21:16:03 +0900

    [level 1] Title: 체육복, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv1/42862. 체육복/체육복.py
+++ 프로그래머스/lv1/42862. 체육복/체육복.py
@@ -1,15 +1,15 @@
 def solution(n, lost, reserve):
     answer = 0
-    lost_ = list(set(lost) - set(reserve))
-    reserve_ = list(set(reserve) - set(lost))
-    lost.sort()
-    reserve.sort()
-    if not lost:
-        return n
-    for l in lost_:
-        n -= 1
-        for r in reserve_:
-            if (abs(l - r) == 1):
-                reserve_.remove(r)
-                n += 1
-    return n
+
+    # 여벌을 가지고 있는 학생이 도난 당할 경우 고려 -> 하나만 도난당했다고 가정하기에 중복을 제거 한다.
+    reserve_del = set(reserve) - set(lost)
+    lost_del = set(lost) - set(reserve)
+    
+    for i in reserve_del:
+        if i-1 in lost_del: # 자신의 앞번호 학생부터 우선순위로 빌려줘야 함
+            lost_del.remove(i-1)
+        elif i+1 in lost_del:
+            lost_del.remove(i+1)
+            
+    answer = n - len(lost_del)
+    return answer
