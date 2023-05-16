24c7d99ac3e1a6e83c4ce9fdaaf08ce6c581dbfe
heesan <75322004+heesankim@users.noreply.github.com>
2023-05-17 00:29:53 +0900

    [level 2] Title: 기능개발, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv2/42586. 기능개발/기능개발.py
+++ 프로그래머스/lv2/42586. 기능개발/기능개발.py
@@ -1,4 +1,26 @@
+# import math
+# def solution(progresses, speeds):
+#     count = 1
+#     answer = []
+#     temp = []
+#     leng = len(progresses)
+#     for i in range(leng):
+#         temp.append(math.ceil((100 - progresses[i]) / speeds[i]))
+#     print(temp)
+    
+#     current = temp[0]
+#     for j in range(1,len(temp)):
+#         if current < temp[j]:
+#             answer.append(count)
+#             count = 1
+#             current = temp[j]
+#         else:
+#             count = count + 1
+#     answer.append(count)
+#     return answer
+
 import math
+
 def solution(progresses, speeds):
     count = 1
     answer = []
@@ -6,26 +28,19 @@ def solution(progresses, speeds):
     leng = len(progresses)
     for i in range(leng):
         temp.append(math.ceil((100 - progresses[i]) / speeds[i]))
+    temp.reverse()
     print(temp)
     
-    current = temp[0]
-    for j in range(1,len(temp)):
-        if current < temp[j]:
+    current = temp.pop()
+    while temp:
+        if current < temp[-1]:
             answer.append(count)
             count = 1
-            current = temp[j]
+            current = temp.pop()
         else:
-            count = count + 1
+            count += 1
+            temp.pop()
     answer.append(count)
     return answer
 
-# # 5 10 1 1 20 1
-# 처음엔 카운트 1 [1,]
-# return 이 1 3 2
-
-
-# 진도가 100%일때 서비스에 반영
-# 뒤에 있는 기능이 앞에 있는 기능보다 먼저 개발 될 수도 있음 이때 뒤에 기능은 앞에 기능을 기다려야함
-# 진도가 적힌 배열 progresses(먼저배포가 되어야 하는 순서) 
-# 각 작업의 개발 속도 speeds 배열
 
