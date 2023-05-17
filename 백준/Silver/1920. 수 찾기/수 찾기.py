0c67f210d3fb08c97bfe075d2513a45977340c55
heesan <75322004+heesankim@users.noreply.github.com>
2023-05-17 19:43:36 +0900

    [Silver IV] Title: 수 찾기, Time: 440 ms, Memory: 47276 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.py
+++ 백준/Silver/1920. 수 찾기/수 찾기.py
@@ -1,31 +1,33 @@
 import sys
+
 input = sys.stdin.readline
 
-n = int(input())
-
-num_list = list(map(int, input().split()))
-
-k = int(input())
+N = int(input())
 
 target_list = list(map(int, input().split()))
 
-sortedNumList = sorted(num_list)
-def num(sortedNumList):
-    for i in target_list:
-        start = 0
-        end = len(sortedNumList) - 1
-        while start <= end:
-            mid = (start + end) // 2
-            if i == sortedNumList[mid]:
-                # i == mid 가 아니라 i = sortedNumList[mid] (인덱스로 접근해야함)
-                print(1)
-                break
-            elif i < sortedNumList[mid]:
-                end = mid - 1  # 얘는 숫자임
-            elif i > sortedNumList[mid]:
-                start = mid + 1  # 얘는 숫자임
+M = int(input())
+
+num_list = list(map(int, input().split()))
+
+sorted_list = sorted(target_list)
+
+def findSame(i, arr):
+    start = 0
+    end = len(arr) - 1
+    while start <= end:
+        mid = (start+end) // 2
+        if i == arr[mid]:
+            return True
+        elif i < arr[mid]:
+            end = mid - 1
+        elif i > arr[mid]:
+            start = mid + 1
         else:
-            print(0)
+            return False
 
-
-num(sortedNumList)
+for i in num_list:
+    if findSame(i, sorted_list) == True:
+        print(1)
+    else:
+        print(0)
